311f097871c3a7f2f040c7517bab7ba5650ba0ec
KadekChandra077 <145751804+KadekChandra077@users.noreply.github.com>
2024-05-02 22:03:47 +0800

    Update LinkkedList2.cpp

--- Linked List/LinkkedList2.cpp
+++ Linked List/LinkkedList2.cpp
@@ -1,73 +1,105 @@
 #include <iostream>
+
 using namespace std;
 
-class Node
-{
-	public:
-	char id;
-	string nama;
-	int urutan;
-	Node *next;
-	
-	Node()
-	{
-		next = NULL;
-	}
+class Node {
+public:
+    char id;
+    Node* next;
+
+    Node() {
+        next = NULL;
+    }
 };
- class LinkedList
- {
- 	public:
- 		Node *head;
- 		Node *tail;
- 		
- 		LinkedList()
- 		{
- 			head = tail = NULL;
-		}
- };
-		void insertDataAfter(char data,char after){
-			if(head==NULL)
-				cout << "ERROR: linkListed Kosong"<< endl;
-			else if(tail->id==after){
-				insertToTail(data);
-			} else{
-				Node *temp = head;
-				
-				while(tmp->id!=after && tmp!=NULL){
-					tmp = tmp->next;
-				}
-				
-				if(tmp==NULL)
-				cout << "ERROR: 'after' Tidak ditemukan" << endl;
-				else{
-					Node *newNode = new Node();
-					newNode->id = data;
-					
-					newNode->next = tmp->next
-					tmp->next = newNode;
-				}
-			}
-		}
- 
- int main(int argc, char** argv)
- {
- 	LinkedList *list1 = new LinkedList();
- 	
- 	Node *node1 = new Node();
- 	node1->id = 'A';
- 	
- 	list1->head = node1;
- 	list1->tail = node1;
- 	
- 	Node *node2 = new Node();
- 	node2->id = 'B';
- 	
- 	list1->head->next = node2;
- 	list1->tail = node2;
- 	
- 	cout << list1->head->id << endl;
- 	cout << list1->head->next->id;
- 	
- 	return 0;
- }
- 
+
+class LinkedList {
+public:
+    Node* head;
+    Node* tail;
+
+    LinkedList() {
+        head = tail = NULL;
+    }
+
+    void insertToHead(char data) {
+        Node* node = new Node();
+        node->id = data;
+
+        if (head == NULL) {
+            head = tail = node;
+        } else {
+            Node* tmp = head;
+            head = node;
+            node->next = tmp;
+        }
+    }
+
+    void insertToTail(char data) {
+        Node* node = new Node();
+        node->id = data;
+
+        if (head == NULL) {
+            head = tail = node;
+        } else {
+            tail->next = node;
+            tail = node;
+        }
+    }
+
+    void traversal() {
+        Node* tmp = head;
+        while (tmp != NULL) {
+            cout << tmp->id << " ";
+            tmp = tmp->next;
+        }
+        cout << endl;
+    }
+
+    void insertDataAfter(char data, char after) {
+        if (head == NULL) {
+            cout << "ERROR: LinkedList Empty" << endl;
+        } else {
+            Node* tmp = head;
+            while (tmp != NULL && tmp->id != after) {
+                tmp = tmp->next;
+            }
+            if (tmp == NULL) {
+                cout << "ERROR: Node with id '" << after << "' not found" << endl;
+            } else {
+                Node* newNode = new Node();
+                newNode->id = data;
+                newNode->next = tmp->next;
+                tmp->next = newNode;
+                if (tmp == tail) {
+                    tail = newNode;
+                }
+            }
+        }
+    }
+};
+
+int main(int argc, char** argv) {
+
+    LinkedList* list1 = new LinkedList();
+
+    list1->insertToHead('A');
+    list1->insertToTail('B');
+
+    cout << list1->head->id << endl;
+    cout << list1->head->next->id << endl;
+
+    Node* nodeX = new Node();
+    nodeX->id = 'X';
+
+    Node* tmp = list1->head->next;
+    list1->head->next = nodeX;
+    nodeX->next = tmp;
+
+    list1->insertToTail('C');
+    list1->insertToTail('D');
+
+    cout << list1->head->id << endl;
+    cout << list1->tail->id << endl;
+
+    return 0;
+}
